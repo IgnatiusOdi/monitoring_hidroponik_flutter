23806ecd58ff61649a9964c721721d7246934b39
Ignatius Odi <82990060+IgnatiusOdi@users.noreply.github.com>
2024-10-16 15:45:48 +0700

    combine fcm popup notification message

--- functions/index.js
+++ functions/index.js
@@ -25,39 +25,22 @@ exports.logData = onValueCreated(
     const suhu = data.split(",")[2]
     log(`pH: ${ph}, PPM: ${ppm}, Suhu: ${suhu}`)
 
-    let payload = []
+    let body = ""
+    if (ph != 0 && (ph < 5.5 || ph > 6.5)) body += `pH ${node}: ${ph}\n`
+    if (ppm < 560) body += `Kadar PPM ${node}: ${ppm}\n`
+    if (suhu < 20 || suhu > 30) body += `Suhu Air ${node}: ${suhu}\n`
+    if (body.length > 0) {
+      body += "Segera lakukan penyesuaian terhadap data sensor terkait!"
 
-    if (ph < 5.5 || ph > 6.5) {
-      payload.push({
+      // FCM
+      messaging.send({
         notification: {
-          title: `pH Air ${node}: ${ph}`,
-          body: `Segera lakukan kontrol pH air pada ${node}!`,
+          title: `Data Sensor ${node}`,
+          body,
         },
         topic: "hidroponik-flutter",
       })
     }
-
-    if (ppm < 560) {
-      payload.push({
-        notification: {
-          title: `Kadar PPM Air ${node}: ${ppm}`,
-          body: `Segera tambahkan nutrisi pada ${node}!`,
-        },
-        topic: "hidroponik-flutter",
-      })
-    }
-
-    if (suhu < 25 || suhu > 30) {
-      payload.push({
-        notification: {
-          title: `Suhu Air ${node}: ${suhu}`,
-          body: `Segera lakukan kontrol suhu air pada ${node}!`,
-        },
-        topic: "hidroponik-flutter",
-      })
-    }
-
-    if (payload.length > 0) messaging.sendEach(payload)
   }
 )
 
@@ -74,24 +57,20 @@ exports.logTinggiAir = onValueUpdated(
       log(`Tinggi Air ${node}: ${tinggiAir}`)
 
       // FCM
-      const payload = {
+      messaging.send({
         notification: {
           title: `Tinggi Air ${node}: ${tinggiAir}`,
           body: `Segera tambahkan air pada ${node}!`,
         },
         topic: "hidroponik-flutter",
-      }
-      messaging.send(payload)
+      })
     }
   }
 )
 
 exports.crontabFunction = onSchedule("0 0 * * *", async (event) => {
   const db = getDatabase()
-  let payload = []
-
   const date = new Date().getTime() + (7 * 60 * 60 * 1000)
-
   log(`Now: ${new Date(date).toLocaleDateString()}`)
 
   const penambahanPPM1 = (await db.ref("/node1/penambahanPPM").get()).val()
@@ -100,21 +79,17 @@ exports.crontabFunction = onSchedule("0 0 * * *", async (event) => {
   const diff2 = Math.floor(Math.abs(date - Date.parse(penambahanPPM2)) / (1000 * 60 * 60 * 24))
   log(`penambahanPPM node1: ${diff1}, penambahanPPM node2: ${diff2}`)
 
-  if (diff1 >= 7) {
-    payload.push({
-      notification: {
-        title: "Penambahan Nutrisi node1",
-        body: "Sudah lewat 7 hari, cek kembali nutrisi pada node1!",
-      },
-      topic: "hidroponik-flutter",
-    })
-  }
+  let payload = []
+  let body = ""
 
-  if (diff2 >= 7) {
+  if (diff1 >= 7) body += "Sudah lewat 7 hari, cek kembali nutrisi pada node1"
+  if (diff2 >= 7) body.length > 0 ? body += " dan node2" : body += "Sudah lewat 7 hari, cek kembali nutrisi pada node2"
+  if (body.length > 0) {
+    body += "!"
     payload.push({
       notification: {
-        title: "Penambahan Nutrisi node2",
-        body: "Sudah lewat 7 hari, cek kembali nutrisi pada node2!",
+        title: "Tambah Nutrisi",
+        body
       },
       topic: "hidroponik-flutter",
     })
@@ -123,24 +98,18 @@ exports.crontabFunction = onSchedule("0 0 * * *", async (event) => {
   const panen1 = (await db.ref("/node1/tanaman/panen1").get()).val()
   const panen2 = (await db.ref("/node2/tanaman/panen1").get()).val()
   const d1 = Math.floor(Math.abs(date - Date.parse(panen1)) / (1000 * 60 * 60 * 24))
-  const d2 = Math.floor(Math.abs(date- Date.parse(panen2)) / (1000 * 60 * 60 * 24))
+  const d2 = Math.floor(Math.abs(date - Date.parse(panen2)) / (1000 * 60 * 60 * 24))
   log(`panen node1: ${d1}, panen node2: ${d2}`)
 
-  if (d1 <= 0) {
+  body = ""
+  if (d1 <= 0) body += "node1"
+  if (d2 <= 0) body.length > 0 ? body += " dan node2" : body += "node2"
+  if (body.length > 0) {
+    body += " sudah bisa dipanen!"
     payload.push({
       notification: {
-        title: "Waktu Panen node1",
-        body: "node1 sudah bisa dipanen!",
-      },
-      topic: "hidroponik-flutter",
-    })
-  }
-
-  if (d2 <= 0) {
-    payload.push({
-      notification: {
-        title: "Waktu Panen node2",
-        body: "node2 sudah bisa dipanen!",
+        title: "Waktu Panen",
+        body
       },
       topic: "hidroponik-flutter",
     })
